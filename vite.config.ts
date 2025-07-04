b1b89f34570a8a03efaa5fe7485f3029f6c449f1
mentsurfari <mentawaiisurfari@gmail.com>
2025-07-04 16:52:58 +0800

    vite build on vercel commit
    vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    // Vercel and other platforms set NODE_ENV to 'production' for builds.
+    // We need to define `process.env` for our code to work in development and production.
+    'process.env': process.env
+  }
+})
